1e0448e002cafe8ad5756c5fdfe5b60a7f089e47
marodriguezd <migueadali@gmail.com>
2024-11-10 00:00:01 +0100

    Demo jugable

---
+++ Proyectos/tuprimerjuego/Character/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	animation.play("idle")
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	update_animation()
+	move_and_slide()
+
+func update_animation():
+	if velocity.x > 0:
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		sprite.flip_h = false
+	
+	
+	if velocity.x != 0:
+		animation.play("walk")
+	else:
+		animation.play("idle")
